f127d145f2e9d868d1758f747a6232f2038127e4
eric-yyjau <eric.yyjau@gmail.com>
2020-02-17 12:30:39 -0800

    test_ok

--- scripts/run_pose_test.sh
+++ scripts/run_pose_test.sh
@@ -1,6 +1,8 @@
-KITIT_VO=/media/bjw/Disk/Dataset/kitti_odom/
+# KITIT_VO=/media/bjw/Disk/Dataset/kitti_odom/
 
-POSE_NET=~/Research/SC-Models/cs+k_pose.tar
+KITIT_VO=/media/yoyee/Big_re/kitti/
+
+POSE_NET=./pretrained/pose/cs+k_pose.tar
 
 python test_pose.py $POSE_NET \
 --img-height 256 --img-width 832 \
--- scripts/run_vo_test.sh
+++ scripts/run_vo_test.sh
@@ -1,14 +1,20 @@
-DATASET_DIR=/media/bjw/Disk/Dataset/kitti_odom/sequences/
-OUTPUT_DIR=results/vo/cs+k_pose/
+seq=10
 
-POSE_NET=~/Research/SC-Models/cs+k_pose.tar
+DATASET_DIR=/media/yoyee/Big_re/kitti/sequences/
+OUTPUT_DIR=results/vo/cs+k_pose/$seq
+
+POSE_NET=./pretrained/pose/cs+k_pose.tar
 
 # save the visual odometry results to "results_dir/09.txt"
-python test_vo.py \
---img-height 256 --img-width 832 \
---sequence 09 \
---pretrained-posenet $POSE_NET --dataset-dir $DATASET_DIR --output-dir $OUTPUT_DIR
+
+#python test_vo.py \
+#--img-height 256 --img-width 832 \
+#--sequence $seq \
+#--pretrained-posenet $POSE_NET --dataset-dir $DATASET_DIR --output-dir $OUTPUT_DIR
 
 # show the trajectory with gt. note that use "-s" for global scale alignment
-evo_traj kitti -s $OUTPUT_DIR/09.txt --ref=./kitti_eval/09.txt -p --plot_mode=xz
+ evo_traj kitti -s $OUTPUT_DIR/$seq.txt --ref=./kitti_eval/$seq.txt -s -p --plot_mode=xz --save_plot $OUTPUT_DIR/traj.zip
+ evo_ape kitti -s ./kitti_eval/$seq.txt $OUTPUT_DIR/$seq.txt -p --plot_mode=xz --save_results $OUTPUT_DIR/ate.zip
+ evo_rpe kitti -s ./kitti_eval/$seq.txt $OUTPUT_DIR/$seq.txt -p --plot_mode=xz --save_results $OUTPUT_DIR/rpe.zip
 
+#python /home/yoyee/Documents/deep_keyframe/kitti-odom-eval/eval_odom.py --result $OUTPUT_DIR --align 7dof --seqs $seq
