bee7e33af5278eb1077a4a29986c239bcbeb25f7
eric-yyjau <eric.yyjau@gmail.com>
2020-03-03 15:06:28 -0800

    update script

--- scripts/run_prepare_data.sh
+++ scripts/run_prepare_data.sh
@@ -1,9 +1,9 @@
 # for kitti raw dataset
 ### on theia
-DATASET=/data/kitti/raw/
-TRAIN_SET=/newfoundland/yyjau/kitti/scsfm_dump/kitti_256/
-STATIC_FILES=data/static_frames.txt
-python data/prepare_train_data.py $DATASET --dataset-format 'kitti_raw' --dump-root $TRAIN_SET --width 832 --height 256 --num-threads 4 --static-frames $STATIC_FILES --with-depth 
+# DATASET=/data/kitti/raw/
+# TRAIN_SET=/newfoundland/yyjau/kitti/scsfm_dump/kitti_256/
+# STATIC_FILES=data/static_frames.txt
+# python data/prepare_train_data.py $DATASET --dataset-format 'kitti_raw' --dump-root $TRAIN_SET --width 832 --height 256 --num-threads 4 --static-frames $STATIC_FILES --with-depth 
 
 # # for cityscapes dataset
 # DATASET=/media/bjw/Disk/Dataset/cityscapes/
@@ -11,6 +11,7 @@ python data/prepare_train_data.py $DATASET --dataset-format 'kitti_raw' --dump-r
 # python data/prepare_train_data.py $DATASET --dataset-format 'cityscapes' --dump-root $TRAIN_SET --width 832 --height 342 --num-threads 4
 
 # # for kitti odometry dataset
-# DATASET=/media/bjw/Disk/Dataset/kitti_odom/
-# TRAIN_SET=/media/bjw/Disk/Dataset/kitti_vo_256/
-# python data/prepare_train_data.py $DATASET --dataset-format 'kitti_odom' --dump-root $TRAIN_SET --width 832 --height 256 --num-threads 4
+### on theia
+DATASET=/data/kitti/odometry/sequences/
+TRAIN_SET=/newfoundland/yyjau/kitti/scsfm_dump/kitti_vo_256/
+python data/prepare_train_data.py $DATASET --dataset-format 'kitti_odom' --dump-root $TRAIN_SET --width 832 --height 256 --num-threads 4
