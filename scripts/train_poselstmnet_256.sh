1f76e20414fde1f53e76ef1a238c6bbf63db0522
eric-yyjau <eric.yyjau@gmail.com>
2020-03-24 13:03:36 -0700

    fix training inv pose bug

--- scripts/train_poselstmnet_256.sh
+++ scripts/train_poselstmnet_256.sh
@@ -6,10 +6,24 @@ python train.py $TRAIN_SET \
 -b4 -s0.1 -c0.5 --epoch-size 1000 --sequence-length 11 \
 --with-mask \
 --with-ssim \
---name poselstmnet_256_v0.2_2fr \
+--name poselstmnet_256_v0.2_2fr_lr3 \
 --batch-size 2 \
+--lr 1e-3 \
 --lstm \
 --dataParallel \
+
+# TRAIN_SET=/newfoundland/yyjau/kitti/scsfm_dump/kitti_vo_256/
+# python train.py $TRAIN_SET \
+# --dispnet DispResNet \
+# --num-scales 1 \
+# -b4 -s0.1 -c0.5 --epoch-size 1000 --sequence-length 5 \
+# --with-mask \
+# --with-ssim \
+# --name poselstmnet_256_v0.2_2fr_seq5 \
+# --batch-size 4 \
+# --lr 1e-4 \
+# --lstm \
+# --dataParallel \
 # --debug \
 # --keyframe ./datasets/kitti_keyframe/orbslam2_key/
 # --pretrained-disp checkpoints/posenet_256_seqLen7/80/dispnet_model_best.pth.tar \
